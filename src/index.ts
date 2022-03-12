cdb15359ca010f8eaafcdf2339920e558ce6a331
望月祐汰 <mczk9402@gmail.com>
2022-03-12 11:05:38 +0900

    section-011

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
-const sayHello = (name: string) => {
-  return `Hello, ${name}!`;
-};
-
-console.log(sayHello('Michael Jacson'));
-process.stdout.write(sayHello('Michael Jacson'));
-
+// ターミナルに出力させるログ
 const printLine = (text: string, breakeLine: boolean = true) => {
   process.stdout.write(text + (breakeLine ? '\n' : ''));
 };
+
+const proptInput = async (text: string) => {
+  printLine(`\n${text}\n`, false);
+  const input: string = await new Promise((resolve) => {
+    return process.stdin.once('data', (data) => {
+      return resolve(data.toString());
+    });
+  });
+
+  return input.trim();
+};
+
+(async () => {
+  const name = await proptInput('名前を入力してください');
+  console.log(name);
+  const age = await proptInput('年齢を入力してください');
+  console.log(age);
+  process.exit();
+})();
